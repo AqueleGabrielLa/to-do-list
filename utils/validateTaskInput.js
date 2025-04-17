fb3655e5999d2dd2f345121144236ccbac398af5
Gab <garcanjo938@gmail.com>
2025-04-17 11:31:24 -0300

    fix: update logic in validateTaskInput

--- utils/validateTaskInput.js
+++ utils/validateTaskInput.js
@@ -3,16 +3,17 @@ const { sendErrorResponse } = require('./response')
 const validStatus = ['pending', 'in_progress', 'done']
 
 function validateTaskInput(res, { title, statusTask, dueDate }, isUpdate = false){
-    if (isUpdate && !title) {
-        return sendErrorResponse(res, 400, 'Dados insuficientes, favor preencher título')
-    }
-
+    
     if (statusTask && !validStatus.includes(statusTask)) {
         return sendErrorResponse(res, 400, 'Status inválido, Valores válidos: pending, in_progress, completed')
     }
 
     if (dueDate && isNaN(Date.parse(dueDate))) {
         return sendErrorResponse(res, 400, 'Data de vencimento inválida. Por favor, insira uma data válida no formato YYYY-MM-DD.')
+    }
+
+    if (!isUpdate && !title) {
+        return sendErrorResponse(res, 400, 'Dados insuficientes, favor preencher título')
     }
 
     return null;
